ab3deb86048ad4d951706fa4b6ec7b360f6a2996
zigreus <wonjong010@naver.com>
2020-06-19 01:05:37 +0900

    실행빠르게 하기위한 배치파일 추가

---
+++ Rnd_Client.bat
@@ -0,0 +1,2 @@
+"C:\Github\UnrealEngine\Engine\Binaries\Win64\UE4Editor.exe" "C:\Github\ServerAndLobbySystem\RnD.uproject" -game -ResX=1200 -ResY=675 -log -WINDOWED
+GOTO:EOF
---
+++ Rnd_Dedicated Server.bat
@@ -0,0 +1,2 @@
+"C:\Github\UnrealEngine\Engine\Binaries\Win64\UE4Editor.exe" "C:\Github\ServerAndLobbySystem\RnD.uproject" -server -game -log -force_steamclient_link -LOG=Server.log
+GOTO:EOF
